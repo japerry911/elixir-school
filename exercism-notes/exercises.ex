705cd39dca2742f00dd0dc1c7e1d85563d5e4623
Jack Perry <jack.e.perry@outlook.com>
2022-01-01 16:04:46 -0700

    👌 IMPROVE: more exercism exercises added

--- exercism-notes/exercises.ex
+++ exercism-notes/exercises.ex
@@ -78,3 +78,27 @@ defmodule GuessingGame do
     "Make a guess"
   end
 end
+
+defmodule GuessingGame do
+  def compare(secret_number, guess \\ :no_guess)
+
+  def compare(secret_number, guess) when secret_number == guess do
+    "Correct"
+  end
+
+  def compare(secret_number, guess) when guess != :no_guess and abs(secret_number - guess) <= 1 do
+    "So close"
+  end
+
+  def compare(secret_number, guess) when guess != :no_guess and guess > secret_number do
+    "Too high"
+  end
+
+  def compare(secret_number, guess) when guess != :no_guess and guess < secret_number do
+    "Too low"
+  end
+
+  def compare(secret_number, guess) when guess == :no_guess do
+    "Make a guess"
+  end
+end
