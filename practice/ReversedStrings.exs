354cba233fbd27d1b4ca0044643fb1b5ee98da51
Jack Perry <jack.e.perry@outlook.com>
2022-01-01 10:14:58 -0700

    👌 IMPROVE: more simplified solution

--- practice/ReversedStrings.exs
+++ practice/ReversedStrings.exs
@@ -1,10 +1,14 @@
+# defmodule ReversedStrings do
+#   def reverse(string) do
+#     String.split(string, "")
+#     |> Enum.filter(&(&1 != ""))
+#     |> Enum.reverse
+#     |> List.to_string
+#   end
+# end
+
 defmodule ReversedStrings do
-  def reverse(string) do
-    String.split(string, "")
-    |> Enum.filter(&(&1 != ""))
-    |> Enum.reverse
-    |> List.to_string
-  end
+  def reverse(string), do: String.reverse(string)
 end
 
 IO.inspect ReversedStrings.reverse("world")
