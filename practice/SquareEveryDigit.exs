33cab05c3acad039b0d09145d64c3391a43b8eb8
Jack Perry <jack.e.perry@outlook.com>
2022-01-01 09:21:14 -0700

    👌 IMPROVE: problem completed

--- practice/SquareEveryDigit.exs
+++ practice/SquareEveryDigit.exs
@@ -1,5 +1,9 @@
 defmodule SquareEveryDigit do
   def square_digits(n) do
+    Integer.digits(n)
+    |> Enum.map(&(trunc(:math.pow(&1, 2))))
+    |> Enum.join
+    |> String.to_integer
   end
 end
 
