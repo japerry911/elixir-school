1e400ca1634b8c622d7e0d322e029cf39032ce13
Jack Perry <jack.e.perry@outlook.com>
2022-01-01 10:11:03 -0700

    👌 IMPROVE: completed problem

--- practice/PrinterErrors.exs
+++ practice/PrinterErrors.exs
@@ -1,8 +1,12 @@
 defmodule PrinterErrors do
-  def printer_error(s) do
+  @invalid_letters Enum.filter(String.split(List.to_string(Enum.to_list(?n..?z)), ""), &(&1 != ""))
 
+  def printer_error(s) do
+    total = length(Enum.filter(String.split(s, ""), &(&1 != "")))
+    errors = length(Enum.filter(String.split(s, ""), &(Enum.member?(@invalid_letters, &1))))
+    "#{errors}/#{total}"
   end
 end
 
-IO.inspect PrinterErrors.printer_error("aaaxbbbbyyhwawiwjjjwwm")
-# "8/22"
+IO.inspect PrinterErrors.printer_error("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
+# "3/56"
