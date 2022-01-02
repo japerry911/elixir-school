125fe9e39f33386a5cf456840bf299905e0155c4
Jack Perry <jack.e.perry@outlook.com>
2022-01-01 20:36:03 -0700

    👌 IMPROVE: more exercism exercises added

--- exercism-notes/exercises.ex
+++ exercism-notes/exercises.ex
@@ -149,7 +149,7 @@ defmodule HighSchoolSweetheart do
     #  **         ** **         **
     # **            *            **
     # **                         **
-    # **     X. X.  +  X. X.     **
+    # **     J. P.  +  C. H.     **
     #  **                       **
     #    **                   **
     #      **               **
@@ -164,4 +164,21 @@ defmodule HighSchoolSweetheart do
 end
 
 
-IO.inspect(HighSchoolSweetheart.pair("Jack Perry", "Cassidy Humphrey"))
+defmodule BirdCount do
+  def today([]), do: nil
+  def today([head | _tail]), do: head
+
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
+
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | tail]), do: true
+  def has_day_without_birds?([head | tail]), do: false or has_day_without_birds?(tail)
+
+  def total([]), do: 0
+  def total([head | tail]), do: head + total(tail)
+
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head >= 5, do: 1 + busy_days(tail)
+  def busy_days([head | tail]) when head < 5, do: 0 + busy_days(tail)
+end
