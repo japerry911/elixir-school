0c8dc94879c90b66bfae67784ae6b352f46b34c6
Jack Perry <jack.e.perry@outlook.com>
2022-01-02 05:50:35 -0700

    👌 IMPROVE: more exercism exercises added

--- exercism-notes/exercises.ex
+++ exercism-notes/exercises.ex
@@ -299,3 +299,34 @@ defmodule NameBadge do
     end
   end
 end
+
+
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+  end
+
+  def ask_name() do
+    String.trim(IO.gets("What is your character's name?\n"))
+  end
+
+  def ask_class() do
+    String.trim(IO.gets("What is your character's class?\n"))
+  end
+
+  def ask_level() do
+    String.to_integer(String.trim(IO.gets("What is your character's level?\n")))
+  end
+
+  def run() do
+    welcome()
+
+    name = ask_name()
+    class = ask_class()
+    level = ask_level
+
+    character_map = %{class: class, level: level, name: name}
+
+    IO.inspect(character_map, label: "Your character")
+  end
+end
