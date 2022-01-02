27cc6474492e31a3475779f87a89cd67342ea59c
Jack Perry <jack.e.perry@outlook.com>
2022-01-02 05:34:56 -0700

    👌 IMPROVE: more exercism exercises added

--- exercism-notes/exercises.ex
+++ exercism-notes/exercises.ex
@@ -280,3 +280,22 @@ defmodule Username do
     end
   end
 end
+
+
+defmodule NameBadge do
+  def print(id, name, department) do
+    if id != nil do
+      if department == nil do
+        "[#{id}] - #{name} - OWNER"
+      else
+        "[#{id}] - #{name} - #{String.upcase(department)}"
+      end
+    else
+      if department == nil do
+        "#{name} - OWNER"
+      else
+        "#{name} - #{String.upcase(department)}"
+      end
+    end
+  end
+end
