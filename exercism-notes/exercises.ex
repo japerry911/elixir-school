4c9041cb5f066f1793c7cec60156b0eda2cb49af
Jack Perry <jack.e.perry@outlook.com>
2022-01-01 20:37:09 -0700

    👌 IMPROVE: more exercism exercises added

--- exercism-notes/exercises.ex
+++ exercism-notes/exercises.ex
@@ -172,8 +172,8 @@ defmodule BirdCount do
   def increment_day_count([head | tail]), do: [head + 1 | tail]
 
   def has_day_without_birds?([]), do: false
-  def has_day_without_birds?([0 | tail]), do: true
-  def has_day_without_birds?([head | tail]), do: false or has_day_without_birds?(tail)
+  def has_day_without_birds?([0 | _tail]), do: true
+  def has_day_without_birds?([_head | tail]), do: false or has_day_without_birds?(tail)
 
   def total([]), do: 0
   def total([head | tail]), do: head + total(tail)
