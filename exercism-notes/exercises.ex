6c03b7a3ea2aabd31a79c02ce27b3ec04333bcd0
Jack Perry <jack.e.perry@outlook.com>
2022-01-03 05:43:52 -0700

    👌 IMPROVE: more exercism exercises added

--- exercism-notes/exercises.ex
+++ exercism-notes/exercises.ex
@@ -526,3 +526,14 @@ defmodule Newsletter do
     end
   end
 end
+
+
+defmodule Chessboard do
+  def rank_range(), do: 1..8
+
+  def file_range(), do: ?A..?H
+
+  def ranks(), do: Enum.to_list(rank_range())
+
+  def files(), do: Enum.map(file_range(), &(<<&1>>))
+end
