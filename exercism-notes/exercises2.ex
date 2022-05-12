86b5f196a470d0fa65baff5c39d321eabbd9f10c
Jack Perry <jack.e.perry@outlook.com>
2022-05-12 08:41:17 -0600

    👌 IMPROVE: progress

--- exercism-notes/exercises2.ex
+++ exercism-notes/exercises2.ex
@@ -228,3 +228,28 @@ defmodule KitchenCalculator do
     {unit, get_volume(from_milliliter(to_milliliter(volume_pair), unit))}
   end
 end
+
+
+
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name)
+    |> String.first()
+  end
+
+  def initial(name) do
+    first_letter(name)
+    |> String.upcase()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    String.split(full_name)
+    |> Enum.map(&(initial(&1)))
+    |> Enum.join(" ")
+  end
+
+  def pair(full_name1, full_name2) do
+    "     ******       ******\n   **      **   **      **\n **         ** **         **\n**            *            **\n**                         **\n**     #{initials(full_name1)}  +  #{initials(full_name2)}     **\n **                       **\n   **                   **\n     **               **\n       **           **\n         **       **\n           **   **\n             ***\n              *\n"
+  end
+end
